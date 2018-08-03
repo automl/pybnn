93b4ae70a595e59c6ffaf868c5546f047575d9ee
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-08-03 16:38:34 +0200

    add dngo

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybnn',
+    version='0.0.1',
+    description='Simple Bayesian neural networks',
+    author='Aaron Klein',
+    author_email='kleinaa@cs.uni-freiburg.de',
+    url="https://github.com/automl/pybnn",
+    license='BSD 3-Clause License',
+    classifiers=['Development Status :: 4 - Beta'],
+    packages=find_packages(),
+    python_requires='>=3',
+    install_requires=['torch', 'torchvision', 'numpy', 'emcee', 'scipy'],
+    extras_require={},
+    keywords=['python', 'Bayesian', 'neural networks'],
+)
