aa6a55a26eda5106aceb2a00ad6265a60d9be040
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-08-07 15:51:58 +0200

    add priors ; move training parameters into train()

--- pybnn/bohamiann.py
+++ pybnn/bohamiann.py
@@ -1,43 +1,42 @@
 from itertools import islice
 import logging
 import typing
+import time
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.utils.data as data_utils
+import torch.nn.functional as F
 
 from pybnn.base_model import BaseModel
 from pybnn.util.normalization import zero_mean_unit_var_unnormalization, zero_mean_unit_var_normalization
 from pybnn.util.infinite_dataloader import infinite_dataloader
 from pybnn.sampler.adaptive_sghmc import AdaptiveSGHMC
+from pybnn.priors import weight_prior, log_variance_prior
 
 
-def get_network(input_dimensionality: int) -> torch.nn.Module:
-    class AppendLayer(nn.Module):
-        def __init__(self, bias=True, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            if bias:
-                self.bias = nn.Parameter(torch.Tensor(1, 1))
-            else:
-                self.register_parameter('bias', None)
+def get_default_network(input_dimensionality: int) -> torch.nn.Module:
+    class Net(nn.Module):
+        def __init__(self, input_dimensionality):
+            super(Net, self).__init__()
+            self.fc1 = nn.Linear(input_dimensionality, 50)
+            self.fc2 = nn.Linear(50, 50)
+            self.fc3 = nn.Linear(50, 50)
+            self.mean = nn.Linear(50, 1)
+            self.log_var = nn.Linear(50, 1)
 
         def forward(self, x):
-            return torch.cat((x, self.bias * torch.ones_like(x)), dim=1)
+            x = F.tanh(self.fc1(x))
+            x = F.tanh(self.fc2(x))
+            x = F.tanh(self.fc3(x))
 
-    def init_weights(module):
-        if type(module) == AppendLayer:
-            nn.init.constant_(module.bias, val=np.log(1e-3))
-        elif type(module) == nn.Linear:
-            nn.init.kaiming_normal_(module.weight, mode="fan_in", nonlinearity="linear")
-            nn.init.constant_(module.bias, val=0.0)
+            m = self.mean(x)
+            logvar = 20 * F.sigmoid(self.log_var(x))
 
-    return nn.Sequential(
-        nn.Linear(input_dimensionality, 50), nn.Tanh(),
-        nn.Linear(50, 50), nn.Tanh(),
-        nn.Linear(50, 50), nn.Tanh(),
-        nn.Linear(50, 1),
-        AppendLayer()).apply(init_weights)
+            return torch.cat([m, logvar], dim=1)
+
+    return Net(input_dimensionality)
 
 
 def nll(input, target):
@@ -54,24 +53,15 @@ def nll(input, target):
 
     log_likelihood = log_likelihood / batch_size
 
-    # TODO: add priors
-    #    log_likelihood += (
-    #            self.log_variance_prior(log_prediction_variance) / self.num_datapoints
-    #    )
-    #
-    #    log_likelihood += self.weight_prior(self.parameters) / self.num_datapoints
-
     return -log_likelihood
 
 
 class Bohamiann(BaseModel):
     def __init__(self,
+                 get_network=get_default_network,
                  batch_size=20,
                  normalize_input: bool = True,
                  normalize_output: bool = True,
-                 num_steps: int = 13000,
-                 burn_in_steps: int = 3000,
-                 keep_every: int = 100,
                  metrics=(nn.MSELoss,)
                  ) -> None:
         """ Bayesian Neural Network for regression problems.
@@ -94,63 +84,19 @@ class Bohamiann(BaseModel):
         normalize_input: bool, optional
             Specifies if inputs should be normalized to zero mean and unit variance.
         normalize_output: bool, optional
-            Specifies whether outputs should be unnormalized.
-        num_steps: int, optional
-            Number of sampling steps to perform after burn-in is finished.
-            In total, `num_steps // keep_every` network weights will be sampled.
-            Defaults to `10000`.
-        burn_in_steps: int, optional
-            Number of burn-in steps to perform.
-            This value is passed to the given `optimizer` if it supports special
-            burn-in specific behavior.
-            Networks sampled during burn-in are discarded.
-            Defaults to `3000`.
-        keep_every: int, optional
-            Number of sampling steps (after burn-in) to perform before keeping a sample.
-            In total, `num_steps // keep_every` network weights will be sampled.
-            Defaults to `100`.
+            Specifies whether outputs should be un-normalized.
         """
 
-        assert burn_in_steps >= 0, "Invalid value for amount of burn-in steps -- cannot be negative."
-        assert keep_every >= 1, "Invalid value for `keep_every`. Specify how many " \
-                                "sampling steps to perform before keeping a sample."
-        assert num_steps > burn_in_steps + keep_every, "Not even a single network would be sampled."
         assert batch_size >= 1, "Invalid batch size. Batches must contain at least a single sample."
 
         self.batch_size = batch_size
 
-        self.num_steps = num_steps
-        self.num_burn_in_steps = burn_in_steps
         self.metrics = metrics
-        self.keep_every = keep_every
         self.normalize_input = normalize_input
         self.normalize_output = normalize_output
-
+        self.get_network = get_network
+        self.is_trained = False
         self.sampled_weights = []  # type: typing.List[typing.Tuple[np.ndarray]]
-
-        logging.info("Performing %d iterations" % (self.num_steps))
-
-    def _keep_sample(self, step: int) -> bool:
-        """ Determine if the network weight sample recorded at `step` should be stored.
-            Samples are recorded after burn-in (`step > self.num_burn_in_steps`),
-            and only every `self.keep_every` th step.
-
-        Parameters
-        ----------
-        step: int
-            Current iteration count.
-
-        Returns
-        ----------
-        should_keep: bool
-            Sentinel that is `True` if and only if network weights should be stored at `step`.
-
-        """
-        if step < self.num_burn_in_steps:
-            logging.debug("Skipping burn-in sample, step = %d" % step)
-            return False
-        sample_t = step - self.num_burn_in_steps
-        return sample_t % self.keep_every == 0
 
     @property
     def network_weights(self) -> np.ndarray:
@@ -158,8 +104,8 @@ class Bohamiann(BaseModel):
 
         Returns
         ----------
-        weight_values: np.ndarray
-            Numpy array containing current network weight values.
+        weight_values: tuple
+            Tuple containing current network weight values.
 
         """
         return tuple(
@@ -197,16 +143,39 @@ class Bohamiann(BaseModel):
         for parameter, sample in zip(self.model.parameters(), weights):
             parameter.copy_(torch.from_numpy(sample))
 
-    def train(self, x_train: np.ndarray, y_train: np.ndarray):
-        """ Train a BNN using input datapoints `x_train` with corresponding labels `y_train`.
+    def train(self, x_train: np.ndarray, y_train: np.ndarray,
+              num_steps: int = 13000,
+              keep_every: int = 100,
+              num_burn_in_steps: int = 3000,
+              lr: float = 1e-2,
+              noise: float = 0.,
+              mdecay: float = 0.05,
+              verbose=False):
+
+        """ Train a BNN using input datapoints `x_train` with corresponding targets `y_train`.
         Parameters
         ----------
         x_train : numpy.ndarray (N, D)
             Input training datapoints.
         y_train : numpy.ndarray (N,)
             Input training labels.
+        num_steps: int, optional
+            Number of sampling steps to perform after burn-in is finished.
+            In total, `num_steps // keep_every` network weights will be sampled.
+            Defaults to `10000`.
+        num_burn_in_steps: int, optional
+            Number of burn-in steps to perform.
+            This value is passed to the given `optimizer` if it supports special
+            burn-in specific behavior.
+            Networks sampled during burn-in are discarded.
+            Defaults to `3000`.
+        keep_every: int, optional
+            Number of sampling steps (after burn-in) to perform before keeping a sample.
+            In total, `num_steps // keep_every` network weights will be sampled.
+            Defaults to `100`.
         """
         logging.debug("Training started.")
+        start_time = time.time()
 
         logging.debug("Clearing list of sampled weights.")
         self.sampled_weights.clear()
@@ -217,8 +186,6 @@ class Bohamiann(BaseModel):
             " with % dimensions each." % (num_datapoints, input_dimensionality)
         )
 
-        x_train_ = np.asarray(x_train)
-
         if self.normalize_input:
             logging.debug(
                 "Normalizing training datapoints to "
@@ -226,102 +193,99 @@ class Bohamiann(BaseModel):
             )
             x_train_, self.x_mean, self.x_std = zero_mean_unit_var_normalization(x_train)
 
-        y_train_ = np.asarray(y_train)
+        x_train_ = torch.from_numpy(x_train_).float()
 
         if self.normalize_output:
             logging.debug("Normalizing training labels to zero mean and unit variance.")
             y_train_, self.y_mean, self.y_std = zero_mean_unit_var_normalization(y_train)
 
+        y_train_ = torch.from_numpy(y_train_).float()
+
         train_loader = infinite_dataloader(
             data_utils.DataLoader(
-                data_utils.TensorDataset(
-                    torch.from_numpy(x_train_).float(),
-                    torch.from_numpy(y_train_).float()
-                ),
+                data_utils.TensorDataset(x_train_, y_train_),
                 batch_size=self.batch_size
             )
         )
 
-        self.model = get_network(input_dimensionality=input_dimensionality)
+        self.model = self.get_network(input_dimensionality=input_dimensionality)
 
-        sampler = AdaptiveSGHMC(self.model.parameters(), scale_grad=num_datapoints)
+        sampler = AdaptiveSGHMC(self.model.parameters(),
+                                scale_grad=num_datapoints,
+                                num_burn_in_steps=num_burn_in_steps,
+                                lr=lr,
+                                mdecay=mdecay,
+                                noise=noise)
 
-        batch_generator = islice(enumerate(train_loader), self.num_steps)
+        batch_generator = islice(enumerate(train_loader), num_steps)
 
-        for epoch, (x_batch, y_batch) in batch_generator:
+        for step, (x_batch, y_batch) in batch_generator:
             sampler.zero_grad()
+
             loss = nll(input=self.model(x_batch), target=y_batch)
+            #loss -= log_variance_prior(self.model(x_batch)[:, 1].view((-1, 1))) / num_datapoints
+            #loss -= weight_prior(self.model.parameters()) / num_datapoints
+
             loss.backward()
             sampler.step()
 
-            if self._keep_sample(epoch):
-                logging.debug("Recording sample, epoch = %d " % (epoch))
+            if verbose and step < num_burn_in_steps and step % 512 == 0:
+                total_nll = torch.mean(nll(self.model(x_train_), y_train_)).data.numpy()
+                total_err = torch.mean((self.model(x_train_)[:, 0] - y_train_) ** 2).data.numpy()
+                t = time.time() - start_time
+                print("Step {:8d} : NLL = {:11.4e} MSE = {:.4e} "
+                      "Time = {:5.2f}".format(step, float(total_nll),
+                                              float(total_err), t))
+
+            if verbose and step > num_burn_in_steps and step % 512 == 0:
+                total_nll = torch.mean(nll(self.model(x_train_), y_train_)).data.numpy()
+                total_err = torch.mean((self.model(x_train_)[:, 0] - y_train_) ** 2).data.numpy()
+                t = time.time() - start_time
+
+                print("step {:8d} : NLL = {:11.4e} MSE = {:.4e} "
+                      "Samples= {} Time = {:5.2f}".format(step,
+                                                          float(total_nll),
+                                                          float(total_err),
+                                                          len(self.sampled_weights), t))
+
+            if step > num_burn_in_steps and (step - num_burn_in_steps) % keep_every == 0:
+                logging.debug("Recording sample, step = %d " % step)
                 weights = self.network_weights
                 logging.debug("Sampled weights:\n%s" % str(weights))
+
                 self.sampled_weights.append(weights)
 
         self.is_trained = True
-        return self
 
     def predict(self, x_test: np.ndarray, return_individual_predictions: bool = False):
-        logging.debug("Predicting started.")
         x_test_ = np.asarray(x_test)
 
-        logging.debug(
-            "Processing %d test datapoints "
-            " with %d dimensions each." % x_test_.shape
-        )
-
         if self.normalize_input:
-            logging.debug(
-                "Normalizing test datapoints to "
-                " zero mean and unit variance."
-            )
             x_test_, *_ = zero_mean_unit_var_normalization(x_test, self.x_mean, self.x_std)
 
         def network_predict(x_test_, weights):
-            logging.debug(
-                "Predicting on data:\n%s Using weights:\n%s" % (
-                    str(x_test_), str(weights)
-                )
-            )
             with torch.no_grad():
                 self.network_weights = weights
-                return self.model(torch.from_numpy(x_test_).float()).numpy()[:, 0]
+                return self.model(torch.from_numpy(x_test_).float()).numpy()
 
         logging.debug("Predicting with %d networks." % len(self.sampled_weights))
-        network_outputs = [
+        network_outputs = np.array([
             network_predict(x_test_, weights=weights)
             for weights in self.sampled_weights
-        ]
+        ])
 
-        mean_prediction = np.mean(network_outputs, axis=0)
-        variance_prediction = np.mean((network_outputs - mean_prediction) ** 2, axis=0)
+        mean_prediction = np.mean(network_outputs[:, :, 0], axis=0)
+        # variance_prediction = np.mean((network_outputs[:, 0] - mean_prediction) ** 2, axis=0)
+        # Total variance
+        variance_prediction = np.mean(network_outputs[:, :, 0] ** 2 + network_outputs[:, :, 1],
+                                      axis=0) - mean_prediction ** 2
 
         if self.normalize_output:
-            logging.debug("Unnormalizing predictions.")
-            logging.debug(
-                "Mean of network predictions "
-                "before unnormalization:\n%s" % str(mean_prediction)
-            )
-            logging.debug(
-                "Variance/Uncertainty of network predictions "
-                "before unnormalization:\n%s" % str(variance_prediction)
-            )
 
             mean_prediction = zero_mean_unit_var_unnormalization(
                 mean_prediction, self.y_mean, self.y_std
             )
             variance_prediction *= self.y_std ** 2
-
-            logging.debug(
-                "Mean of network predictions "
-                "after unnormalization:\n%s" % str(mean_prediction)
-            )
-            logging.debug(
-                "Variance/Uncertainty of network predictions "
-                "after unnormalization:\n%s" % str(variance_prediction)
-            )
 
             for i in range(len(network_outputs)):
                 network_outputs[i] = zero_mean_unit_var_unnormalization(
@@ -329,5 +293,5 @@ class Bohamiann(BaseModel):
                 )
 
         if return_individual_predictions:
-            return mean_prediction, variance_prediction, network_outputs
+            return mean_prediction, variance_prediction, network_outputs[:, :, 0]
         return mean_prediction, variance_prediction
---
+++ pybnn/priors.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+import torch
+
+
+def log_variance_prior(log_variance: torch.Tensor, mean: float = 1e-6, variance: float = 0.01) -> torch.Tensor:
+    return torch.mean(
+        torch.sum(
+            ((-((log_variance - torch.log(torch.tensor(mean))) ** 2)) /
+             (2. * variance)) - 0.5 * torch.log(torch.tensor(variance)),
+            dim=1
+        )
+    )
+
+
+def weight_prior(parameters: Iterable[torch.Tensor], wdecay: float = 1.) -> torch.Tensor:
+    num_parameters = torch.sum(torch.tensor([
+        torch.prod(torch.tensor(parameter.size()))
+        for parameter in parameters
+    ]))
+
+    log_likelihood = torch.sum(torch.tensor([
+        torch.sum(-wdecay * 0.5 * (parameter ** 2))
+        for parameter in parameters
+    ]))
+
+    return log_likelihood / (num_parameters.float() + 1e-16)
