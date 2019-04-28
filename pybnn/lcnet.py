3fcc7d36ce5d09fa15049872e3f516c9261d7b3e
Aaron Klein <kleinaa@informatik.uni-freiburg.de>
2019-04-28 12:32:57 +0200

    clean up

--- pybnn/lcnet.py
+++ pybnn/lcnet.py
@@ -3,186 +3,23 @@ from copy import deepcopy
 import numpy as np
 import torch
 import torch.nn as nn
+
 from pybnn.bohamiann import Bohamiann
 from pybnn.util.layers import AppendLayer
 
-"""
-def vapor_pressure(t, a, b, c):
-    a_ = a
-    b_ = b / 10.
-    c_ = c / 10.
 
-    return torch.exp(-a_ - b_ / t - c_ * torch.log(t)) - torch.exp(-a_ - b_)
-
-
-def pow_func(t, a, b):
-    return a * (t ** b - 1)
-
-
-def log_power(t, a, b, c):
-    b_ = b / 10
-
-    return 2 * a * (1 / (1 + torch.exp(-b_ * c)) - 1 / (1 + torch.exp(c * torch.log(t) - b_)))
-
-
-def exponential(t, a, b):
-    b_ = 10 * b
-    return a * (torch.exp(-b_) - torch.exp(-b_ * t))
-
-
-def hill_3(t, a, b, c):
-    return a * (1 / (c ** b * t ** (-b) + 1) - a / (c ** b + 1))
-
-def bf_layer(theta, t):
-    a = theta[:, 0]
-    b = theta[:, 1]
-    c = theta[:, 2]
-    y_a = vapor_pressure(t, a, b, c)
-
-    a = theta[:, 3]
-    b = theta[:, 4]
-    y_b = pow_func(t, a, b)
-
-    a = theta[:, 5]
-    b = theta[:, 6]
-    c = theta[:, 7]
-    y_c = log_power(t, a, b, c)
-
-    a = theta[:, 8]
-    b = theta[:, 9]
-    y_d = exponential(t, a, b)
-
-    a = theta[:, 10]
-    b = theta[:, 11]
-    c = theta[:, 12]
-    y_e = hill_3(t, a, b, c)
-
-    return torch.stack([y_a, y_b, y_c, y_d, y_e], dim=1)
-
-
-
-
-def vapor_pressure(x, a, b, c, *args):
-    a_ = -a
-    b_ = -b / 10.
-    c_ = -c / 10.
-
-    return (torch.exp(a_ + b_ / (x + 1e-5) + c_ * torch.log(x + 1e-5)) - torch.exp(a_ + b_))
-
-
-def log_log_linear(x, a, b, *args):
-    return torch.log(1 + a * torch.log(1 + x) + b)
-
-
-def hill_3(x, a, b, c, *args):
-    return a * (1. / ((c / x) ** b + 1.) - 1. / (c ** b + 1.))
-
-
-def pow_func(x, a, b, *args):
-    return a * ((x) ** b - 1)
-
-
-def log_power(x, a, b, c, *args):
-    b_n = b * 0.1
-    return 2 * a * (1 / (1. + torch.exp(-c * b_n)) - 1 / (1. + (x / torch.exp(b_n)) ** c))
-
-
-def pow_4(x, a, b, c, *args):
-    a_n = a
-    b_n = b / 10.
-    c_n = c / 10.
-    d = 1 + (a_n + b_n) ** (-c_n)
-    return d - (a_n * x + b_n) ** (-c_n)
-
-
-def mmf(x, a, b, c, d, *args):
-    return a - (a - b) / (1 + (c * x) ** d)
-
-
-def exp_4(x, a, b, c, d, *args):
-    f_max = 1 - np.exp(-1)
-    f_min = 0 - np.exp(1)
-    return ((d - torch.exp(-a * x ** c + b)) - f_min) / (f_max - f_min)
-
-
-def janoschek(x, a, b, c, d, *args):
-    # c_n = c ** 2
-    c_n = c
-    return a - (a - b) * torch.exp(-c_n * x ** d)
-
-
-def weibull(x, a, b, c, d, *args):
-    # c_n = c ** 2
-    return a - (a - b) * torch.exp(-(c * x) ** d)
-
-
-def ilog_2(x, a, *args):
-    a_n = a * 0.1
-    b = a_n / np.log(2.1) + 1
-    return b - a_n / torch.log((x + 1.1))
-
-
-def linear(x, bias=0, a=1, *args):
-    return a * x + bias
-
-
-def quadratic(x, bias=0, a=0.1, b=0.1, *args):
-    return a * (x ** 2) + b * x + bias
-
-
-def exponential(x, a, b, *args):
-    return b * (- torch.exp(-10 * a * x) + torch.exp(-10 * a))
-
-
-def bf_layer(theta, t):
-    y_a = vapor_pressure(t, theta[:, 0], theta[:, 1], theta[:, 2])
-    y_b = log_log_linear(t, theta[:, 3], theta[:, 4])
-
-    y_c = hill_3(t, theta[:, 5], theta[:, 6], theta[:, 7])
-
-    y_d = pow_func(t, theta[:, 8], theta[:, 9])
-
-    y_e = log_power(t, theta[:, 10], theta[:, 11], theta[:, 12])
-
-    y_f = pow_4(t, theta[:, 13], theta[:, 14], theta[:, 15])
-
-    y_g = mmf(t, theta[:, 16], theta[:, 17], theta[:, 18], theta[:, 19])
-
-    y_h = exp_4(t, theta[:, 20], theta[:, 21], theta[:, 22], theta[:, 23])
-
-    y_i = janoschek(t, theta[:, 24], theta[:, 25], theta[:, 26], theta[:, 27])
-
-    y_j = weibull(t, theta[:, 28], theta[:, 29], theta[:, 30], theta[:, 31])
-
-    y_k = ilog_2(t, theta[:, 32])
-
-    y_l = linear(t, theta[:, 33], theta[:, 34])
-
-    y_m = quadratic(t, theta[:, 35], theta[:, 36], theta[:, 37])
-
-    y_n = exponential(t, theta[:, 38], theta[:, 39])
-
-    return torch.stack([y_a, y_b, y_c, y_d, y_e, y_f, y_g, y_h, y_i, y_j, y_k, y_l, y_m, y_n], dim=1)
-"""
-
-
-# def vapor_pressure(x, a, b, c, *args):
-#     b_ = (b + 1) / 2 / 100
-#     a_ = (a + 1) / 2
-#     c_ = (c + 1) / 2
-#     return (torch.exp(a_ + b_ / (x + 1e-5) + c_ * torch.log(x))) # - (torch.exp(a_ + b_))
 def vapor_pressure(x, a, b, c, *args):
     b_ = (b + 1) / 2 / 10
     a_ = (a + 1) / 2
     c_ = (c + 1) / 2 / 10
-    return torch.exp(-a_ - b_ / (x + 1e-5) - c_ * torch.log(x))  - (torch.exp(a_ + b_))
+    return torch.exp(-a_ - b_ / (x + 1e-5) - c_ * torch.log(x)) - (torch.exp(a_ + b_))
 
 
 def log_func(t, a, b, c, *args):
     a_ = (a + 1) / 2 * 5
     b_ = (b + 1) / 2
     c_ = (c + 1) / 2 * 10
-    return (c_ + a_ * torch.log(b_ * t + 1e-10)) / 10.  # - (c_ + a_ * torch.log(b_)) / 10.
+    return (c_ + a_ * torch.log(b_ * t + 1e-10)) / 10.
 
 
 def hill_3(x, a, b, c, *args):
@@ -192,17 +29,11 @@ def hill_3(x, a, b, c, *args):
     return a_ * (1. / ((c_ / x + 1e-5) ** b_ + 1.))
 
 
-def linear(x, bias=0, a=1, *args):
-    a_ = (a + 1) / 2
-    return a_ * x + bias
-
-
 def bf_layer(theta, t):
     y_a = vapor_pressure(t, theta[:, 0], theta[:, 1], theta[:, 2])
 
     y_b = log_func(t, theta[:, 3], theta[:, 4], theta[:, 5])
 
-    # y_c = linear(t, theta[:, 6], theta[:, 7])
     y_c = hill_3(t, theta[:, 6], theta[:, 7], theta[:, 8])
 
     return torch.stack([y_a, y_b, y_c], dim=1)
@@ -218,7 +49,6 @@ def get_lc_net_architecture(input_dimensionality: int) -> torch.nn.Module:
             self.theta_layer = nn.Linear(n_hidden, 9)
             self.weight_layer = nn.Linear(n_hidden, 3)
             self.asymptotic_layer = nn.Linear(n_hidden, 1)
-            # self.sigma_layer = nn.Linear(n_hidden, 1)
             self.sigma_layer = AppendLayer(noise=1e-3)
 
         def forward(self, input):
@@ -236,11 +66,7 @@ def get_lc_net_architecture(input_dimensionality: int) -> torch.nn.Module:
             asymptotic = torch.sigmoid(self.asymptotic_layer(x))
 
             mean = residual + asymptotic
-            # mean = residual
             return self.sigma_layer(mean)
-            # std = torch.sigmoid(self.sigma_layer(x))
-
-            # return torch.cat((mean, std), dim=1)
 
     return Architecture(n_inputs=input_dimensionality)
 
